d19cf4208c5a856527108f799c7fe29738dc8b55
asraful-islam <sourob2356@gmail.com>
2025-02-18 01:37:06 +0600

    Add validation functions and calculate sleep time utility

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function calculateVAT(amount){
      
-     if(amount <=0 || typeof amount !== 'number'){
+     if(amount < 0 || typeof amount !== 'number'){
           return 'Invalid'  
      }
      
@@ -35,3 +35,68 @@ function validContact(number){
 // console.log(validContact("01819334a67"))
 
 
+function willSuccess(results){
+     let passedSubjects = 0;
+     let failedSubjects = 0;
+     if(!Array.isArray(results)){
+          return 'Invalid';
+     }
+     for (let i = 0; i < results.length; i++) {
+          if (typeof results[i] !== 'number') {
+               return false;
+          }
+          if(results[i] < 50){
+               failedSubjects++
+          }else{
+               passedSubjects++
+          }
+     }
+
+     if(passedSubjects > failedSubjects){
+          return true;
+     }
+
+     return false;
+     
+}
+// console.log(willSuccess([60, 70, 80, 40, 30]))
+
+
+
+function validProposal(male,female){
+      
+      if(typeof male !== 'object' || typeof female !== 'object'){
+          return 'Invalid'
+      }
+      if(Array.isArray(male) ||  Array.isArray(female)){
+          return 'Invalid'
+      }
+      const ageDiference = Math.abs(male.age - female.age)
+      if(ageDiference > 7 || male.gender === female.gender){
+          return false;
+      }
+
+      return true;
+      
+}
+// console.log(validProposal({ name: "toya", gender: "female", age: 24 },
+//      { name: "bjoy", gender: "male", age: 32 }))
+
+
+function calculateSleepTime(secondsInArry){
+     let timeInSeconds = 0;
+     if(!Array.isArray(secondsInArry)){
+          return 'Invalid'
+     }
+     for(let time of secondsInArry){
+          if(typeof time !== 'number'){
+               return 'Invalid'
+          }
+          timeInSeconds += time
+     }
+     const hours = Math.floor(timeInSeconds / 3600); 
+     const minutes = Math.floor((timeInSeconds % 3600) / 60); 
+     const seconds = timeInSeconds % 60; 
+     return { hour: hours , minute: minutes , second: seconds } ;
+}
+console.log(calculateSleepTime([100, 3800, "90" ]))
