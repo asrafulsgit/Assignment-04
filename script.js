8cb2c538078e0b002e675f90a47056d58f5bf441
asraful-islam <sourob2356@gmail.com>
2025-02-17 23:40:19 +0600

    Add VAT calculation and contact validation functions

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('hello')
+
+
+function calculateVAT(amount){
+     
+     if(amount <=0 || typeof amount !== 'number'){
+          return 'Invalid'  
+     }
+     
+     const vatPercentage = 7.5 ;
+     const vat = amount*vatPercentage/100;
+     return vat;
+}
+// console.log(calculateVAT(1500))
+
+
+function validContact(number){
+     if(typeof number !== 'string' ){
+          return 'Invalid'
+     }
+     
+     if(number.length !== 11 || number[0] !== '0' || number[1] !== '1' || number.includes(' ')){
+          return false;
+     }
+
+     for (let i = 0; i < number.length; i++) {
+          if (isNaN(Number(number[i]))) {
+               return false;
+          }
+      }
+
+     return true;
+
+}
+
+// console.log(validContact("01819334a67"))
+
+
